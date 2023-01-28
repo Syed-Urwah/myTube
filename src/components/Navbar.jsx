5f2117dfe6b87ada730a42d2218dbd6470480d1f
Syed-Urwah <syedurwahdev@gmail.com>
2023-01-28 20:42:54 +0500

    28/1/23

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import homeLogo from "../assets/home-logo.png";
 
 export default function Navbar() {
   return (
-    <div className='w-56 h-screen bg-bg-main'>
+    <div className="pt-3 w-72 h-screen bg-bg-main text-white">
+      <div className="s w-full flex flex-col justify-center">
 
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+
+        <div className="line border-t border-[#272727] mt-5"></div>
+
+        <div className="sign-in">
+          Signin
+        </div>
+
+        <div className="line border-t border-[#272727] mt-5"></div>
+
+        <p className="m-auto w-[50%] my-5">Best of myTube</p>
+
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+
+        <a href="/" className="home w-[80%] flex h-10 justify-center items-center border-solid hover:bg-[#272727] rounded-xl ">
+          <div className="flex justify-center items-end h-1/2">
+            <img className="w-6 h-6 mr-4" src={homeLogo} alt="" />
+            <p>Home</p>
+          </div>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Vidoes.jsx
+++ src/components/Vidoes.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Vidoes() {
   return (
-    <div className='w-max'>
-
+    <div className='w-'>
+      videos
     </div>
   )
 }
